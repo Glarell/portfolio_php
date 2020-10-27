fece49182bbcd02d4bb1280e254ee33e3df5c2ed
PCFIXE <pilou.tondon@hotmail.fr>
2020-10-27 13:08:33 +0100

    add filters

--- portfolio/src/controller/IndexController.php
+++ portfolio/src/controller/IndexController.php
@@ -12,12 +12,18 @@ class IndexController {
 	}
 
 	public static function resultForms() {
-		// TODO FILTRERS
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$tel = $_POST['phone'];
-		$message = $_POST['message'];
-		$footer = "Nom: " . $name . "\n" . "Tel: " . $tel;
-		mail($email, "Sujet Email", $message . "\n" . $footer, "headers");
+		// TODO FILTERS
+		$email = htmlspecialchars($_POST['email']);
+		$tel = htmlspecialchars($_POST['phone']);
+		if (filter_var($email,FILTER_VALIDATE_EMAIL) && filter_var($tel,FILTER_VALIDATE_INT)) {
+			$email = filter_var($email,FILTER_SANITIZE_EMAIL);
+			$tel = filter_var($tel, FILTER_SANITIZE_NUMBER_INT, FILTER_SANITIZE_STRING);
+			$name = htmlspecialchars($_POST['name']);
+			$name = filter_var($name, FILTER_SANITIZE_STRING, FILTER_SANITIZE_NUMBER_INT);
+			$message = htmlspecialchars($_POST['message']);
+			$message = filter_var($name, FILTER_SANITIZE_STRING, FILTER_SANITIZE_SPECIAL_CHARS);
+			$footer = "Nom: " . $name . "\n" . "Tel: " . $tel;
+			mail($email, "Sujet Email", $message . "\n" . $footer, "headers");
+		}
 	}
 }
